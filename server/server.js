c6766cc3e52ca16677d8ca07e4a3df0f90dcc4da
zlin3 <tslzf@hotmail.com>
2018-03-31 09:03:55 -0700

    add form for messages and showing incoming messages

--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user joined'));
 
-  socket.on('createMessage', (newMessage) => {
+  socket.on('createMessage', (newMessage, callback) => {
     console.log('createMessage', newMessage);
 
     io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
-    //socket.broadcast.emit('newMessage', newMessage);
+    callback('this is from the server');
   });
 
   socket.on('disconnect', () => {
