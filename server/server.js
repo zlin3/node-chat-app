2fe9bd8e0fac834108597d10f33f90b61d00e2ed
zlin3 <tslzf@hotmail.com>
2018-03-30 23:58:03 -0700

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,12 +13,22 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage);
     newMessage.createdAt = new Date().getTime();
     io.emit('newMessage', newMessage);
+    //socket.broadcast.emit('newMessage', newMessage);
   });
 
   socket.on('disconnect', () => {
