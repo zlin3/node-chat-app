fbfcc7502871231deb673365024ac9b813c3645c
zlin3 <tslzf@hotmail.com>
2018-03-31 21:22:35 -0700

    send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -72,8 +72,8 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
   var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('');
